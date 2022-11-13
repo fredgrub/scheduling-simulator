d80b86acd0b78162cefdf5db605766c332349095
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2022-11-12 23:43:04 -0300

    Fix reading issue

--- src/swf_reader.py
+++ src/swf_reader.py
@@ -8,23 +8,21 @@ class ReaderSWF:
         self.jobs_info = {'p': [], 'q': [], 'r': []}
 
     def read_and_extract_data(self):
-        try:
-            with open(self.filename, 'r') as reader:
-                for line in reader.readlines():
-                    
-                    row = re.split(" +", line.lstrip(" "))
-                    
-                    if row[0] == ';':
-                        if row[1] == "MaxJobs:":
-                            self.number_of_jobs = int(row[2].rstrip())
-                        elif row[1] == "MaxNodes:":
-                            self.number_of_nodes = int(row[2].rstrip())
-                        continue
-                    
-                    # row[4] is q, row[3] is p, and row[1] is r
-                    if int(row[4]) > 0 and int(row[4]) <= 256 and int(row[3]) > 0:
-                        self.jobs_info['p'].append(int(row[3]))
-                        self.jobs_info['q'].append(int(row[4]))
-                        self.jobs_info['r'].append(int(row[1]))
-        except IOError:
-            print("Problem while reading the file: " + self.filename)
+        with open(self.filename, 'r') as reader:
+            for line in reader.readlines():
+                
+                row = re.split(" +", line.lstrip(" "))
+                row[-1] = row[-1].rstrip()
+
+                if row[0].startswith(";"):
+                    if "MaxJobs:" in row:
+                        self.number_of_jobs = int(row[row.index("MaxJobs:") + 1])
+                    elif "MaxNodes:" in row:
+                        self.number_of_nodes = int(row[row.index("MaxNodes:") + 1])
+                    continue
+
+                # row[4] is q, row[3] is p, and row[1] is r
+                if int(row[4]) > 0 and int(row[4]) <= 256 and int(row[3]) > 0:
+                    self.jobs_info['p'].append(int(row[3]))
+                    self.jobs_info['q'].append(int(row[4]))
+                    self.jobs_info['r'].append(int(row[1]))
--- tests/test_swf_reader.py
+++ tests/test_swf_reader.py
@@ -1,7 +1,6 @@
 import pytest
 import src.swf_reader as swf_reader
 
-
 @pytest.mark.parametrize("workload_file, number_of_jobs, number_of_nodes", [
     ("src/swfs/ANL-Intrepid-2009-1.swf", 68936, 40960),
     ("src/swfs/CEA-Curie-2011-2.1-cln.swf", 773138, 5544),
@@ -16,10 +15,7 @@ import src.swf_reader as swf_reader
 ])
 def test_read_swf(workload_file, number_of_jobs, number_of_nodes):
     reader = swf_reader.ReaderSWF(workload_file)
-    try:
-        reader.read_and_extract_data()
-    except IOError:
-        assert False, "Problem while reading the file: " + reader.filename
+    reader.read_and_extract_data()
     
     assert reader.number_of_jobs == number_of_jobs
     assert reader.number_of_nodes == number_of_nodes
