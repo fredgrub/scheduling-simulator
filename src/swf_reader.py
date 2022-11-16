20eaab4bbccdd328d4889c33ab1cdf90be70d157
Lucas de sousa rosa <54755666+fredgrub@users.noreply.github.com>
2022-11-16 16:59:21 -0300

    Simplify (#1)
    * Add .gitignore
    
    * Update Dockerfile and requirements.txt
    
    * Created tests and updated README
    
    * Update .gitignore
    
    * Fix reading issue
    
    * Refactor simulator, reader. Adjust the tests
    
    * Create devcontainer
    
    * Add support for CLI arguments
    
    * Clear redundant files and organize
    
    * Fix the error that guarantees correctness

--- src/swf_reader.py
+++ src/swf_reader.py
@@ -6,7 +6,6 @@ class ReaderSWF:
         self.number_of_jobs = None
         self.number_of_nodes = None
         self.jobs_info = {'p': [], 'q': [], 'r': []}
-        
         self.read_and_extract_data()
 
     def read_and_extract_data(self):
@@ -14,14 +13,15 @@ class ReaderSWF:
             for line in reader.readlines():
                 
                 row = re.split(" +", line.lstrip(" "))
-                
-                if row[0] == ';':
-                    if row[1] == "MaxJobs:":
-                        self.number_of_jobs = int(row[2].rstrip())
-                    elif row[1] == "MaxNodes:":
-                        self.number_of_nodes = int(row[2].rstrip())
+                row[-1] = row[-1].rstrip()
+
+                if row[0].startswith(";"):
+                    if "MaxJobs:" in row:
+                        self.number_of_jobs = int(row[row.index("MaxJobs:") + 1])
+                    elif "MaxNodes:" in row:
+                        self.number_of_nodes = int(row[row.index("MaxNodes:") + 1])
                     continue
-                
+
                 # row[4] is q, row[3] is p, and row[1] is r
                 if int(row[4]) > 0 and int(row[4]) <= 256 and int(row[3]) > 0:
                     self.jobs_info['p'].append(int(row[3]))
