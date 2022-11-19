f631634ed1291321aa0bb4dafb1661a5503acccf
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2022-11-19 02:59:01 +0000

    Improve the reader and fix some bugs

--- src/swf_reader.py
+++ src/swf_reader.py
@@ -4,26 +4,31 @@ class ReaderSWF:
     def __init__(self, filename):
         self.filename = filename
         self.number_of_jobs = None
-        self.number_of_nodes = None
+        self.number_of_processors = None
         self.jobs_info = {'p': [], 'q': [], 'r': []}
         self.read_and_extract_data()
 
     def read_and_extract_data(self):
         with open(self.filename, 'r') as reader:
+            jobs_counter = 0
             for line in reader.readlines():
                 
                 row = re.split(" +", line.lstrip(" "))
                 row[-1] = row[-1].rstrip()
 
+
                 if row[0].startswith(";"):
-                    if "MaxJobs:" in row:
-                        self.number_of_jobs = int(row[row.index("MaxJobs:") + 1])
+                    if "MaxProcs:" in row:
+                        self.number_of_processors = int(row[row.index("MaxProcs:") + 1])
                     elif "MaxNodes:" in row:
-                        self.number_of_nodes = int(row[row.index("MaxNodes:") + 1])
+                        self.number_of_processors = int(row[row.index("MaxNodes:") + 1])
                     continue
 
                 # row[4] is q, row[3] is p, and row[1] is r
-                if int(row[4]) > 0 and int(row[4]) <= 256 and int(row[3]) > 0:
+                if int(row[4]) > 0 and int(row[4]) <= self.number_of_processors and int(row[3]) > 0:
+                    jobs_counter += 1
                     self.jobs_info['p'].append(int(row[3]))
                     self.jobs_info['q'].append(int(row[4]))
                     self.jobs_info['r'].append(int(row[1]))
+            
+            self.number_of_jobs = jobs_counter
--- tests/test_swf_reader.py
+++ tests/test_swf_reader.py
@@ -1,20 +1,19 @@
 import pytest
 import src.swf_reader as swf_reader
 
-@pytest.mark.parametrize("workload_file, number_of_jobs, number_of_nodes", [
-    ("src/swfs/ANL-Intrepid-2009-1.swf", 68936, 40960),
-    ("src/swfs/CEA-Curie-2011-2.1-cln.swf", 773138, 5544),
-    ("src/swfs/CTC-SP2-1996-3.1-cln.swf", 79302, 338),
-    ("src/swfs/HPC2N-2002-2.2-cln.swf", 527371, 240),
-    ("src/swfs/lublin_256.swf", 100000, 256),
-    ("src/swfs/lublin_1024.swf", 100000, 1024),
-    ("src/swfs/lublin_256_est.swf", 100000, 256),
-    ("src/swfs/lublin_1024_est.swf", 100000, 1024),
-    ("src/swfs/SDSC-BLUE-2000-4.2-cln.swf", 250440, 144),
-    ("src/swfs/SDSC-SP2-1998-4.2-cln.swf", 73496, 128),
+@pytest.mark.parametrize("workload_file, number_of_processors", [
+    ("src/swfs/ANL-Intrepid-2009-1.swf", 163840),
+    ("src/swfs/CEA-Curie-2011-2.1-cln.swf", 93312),
+    ("src/swfs/CTC-SP2-1996-3.1-cln.swf", 338),
+    ("src/swfs/HPC2N-2002-2.2-cln.swf", 240),
+    ("src/swfs/lublin_256.swf", 256),
+    ("src/swfs/lublin_1024.swf", 1024),
+    ("src/swfs/lublin_256_est.swf", 256),
+    ("src/swfs/lublin_1024_est.swf", 1024),
+    ("src/swfs/SDSC-BLUE-2000-4.2-cln.swf", 1152),
+    ("src/swfs/SDSC-SP2-1998-4.2-cln.swf", 128),
 ])
-def test_read_swf(workload_file, number_of_jobs, number_of_nodes):
+def test_read_swf(workload_file, number_of_processors):
     reader = swf_reader.ReaderSWF(workload_file)
     
-    assert reader.number_of_jobs == number_of_jobs
-    assert reader.number_of_nodes == number_of_nodes
+    assert reader.number_of_processors == number_of_processors
