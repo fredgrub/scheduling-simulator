f42ef3bd1923a6eed3b0c81591aa8c199216cd02
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2022-11-27 13:21:21 +0000

    Make reader read estimated processing time

--- src/simulator/swf_reader.py
+++ src/simulator/swf_reader.py
@@ -5,7 +5,7 @@ class ReaderSWF:
         self.filename = filename
         self.number_of_jobs = None
         self.number_of_processors = None
-        self.jobs_info = {'p': [], 'q': [], 'r': []}
+        self.jobs_info = {'p': [], '~p': [], 'q': [], 'r': []}
         self.read_and_extract_data()
 
     def read_and_extract_data(self):
@@ -24,10 +24,11 @@ class ReaderSWF:
                         self.number_of_processors = int(row[row.index("MaxNodes:") + 1])
                     continue
 
-                # row[4] is q, row[3] is p, and row[1] is r
-                if int(row[4]) > 0 and int(row[4]) <= self.number_of_processors and int(row[3]) > 0:
+                # row[4] is q, row[3] is p, row[8] is ~p, and row[1] is r
+                if int(row[4]) > 0 and int(row[4]) <= self.number_of_processors and int(row[3]) > 0 and int(row[8]) > 0:
                     jobs_counter += 1
                     self.jobs_info['p'].append(int(row[3]))
+                    self.jobs_info['~p'].appen(int(row[8]))
                     self.jobs_info['q'].append(int(row[4]))
                     self.jobs_info['r'].append(int(row[1]))
             
