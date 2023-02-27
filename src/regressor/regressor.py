1b4c63038526b922bdca7801ef9509063cdf4dc3
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2023-02-27 15:56:27 +0100

    Prettier outputs & QoL changes

--- src/regressor/regressor.py
+++ src/regressor/regressor.py
@@ -1,68 +1,232 @@
+import json
+import gdown
 import numpy as np
-from polynomials import lin, qdr, cub, qua, qui, sex
+import os
 from scipy.optimize import curve_fit
+from polynomials import *
+
+DOWNLOADABLE_DATA = [
+    {
+        "name": "Score distribution (Lublin 256 workload)",
+        "file-name": "lublin-256-final-score.csv",
+        "file-id": "1g4DvCxirJ120mTJbqsHI_kfCp67I9gZN",
+    },
+    {
+        "name": "Score distribution (CTC-SP2 workload)",
+        "file-name": "ctc-sp2-final-score.csv",
+        "file-id": "1-5KPPduIMoVJyGnq9LpVa-1QqF_EqLd3",
+    },
+    {
+        "name": "Score distribution (SDSC-Blue workload)",
+        "file-name": "sdsc-blue-final-score.csv",
+        "file-id": "1-7fQVRNCo6vvpDxsflOALFHwp4B8zRvV",
+    },
+]
+
+SCORE_DISTRIBUTION = "lublin-256-final-score.csv"
+REPORT_FILE = "regression_report.json"
+FUNCTIONS = [lin, qdr, cub, qua, qui, sex]
+
 
 class Regressor:
-    def __init__(self, filename, poly):
-        self.polynomial = poly
-        self.p, self.q, self.r, self.score = self._read_score_data(filename)
-        self.number_of_samples = len(self.score)
-        
-        self.popt = None
-        self.pcov = None
-        self.predicted_y = None
+    """
+    A class to perform a multiple linear regression on a given dataset
+    using a set of functions.
+    """
 
-    def _read_score_data(self, filename):
-        p, q, r, score = [[] for _ in range(4)]
+    def __init__(self, data_file, functions):
+        """
+        Initialize the regressor.
 
-        with open(filename) as f:
-            for line in f.readlines():
-                row = line.split(',')
-                p.append(int(row[0]))
-                q.append(int(row[1]))
-                r.append(int(row[2]))
-                score.append(float(row[3]))
-        
-        return [np.array(element) for element in (p, q, r, score)]
+        Parameters
+        ----------
+        data_file : str
+            The path to the file containing the data set.
+        functions : list
+            The list of functions to use for the regression.
+        """
+        self.functions = functions
+        self.data_set = self._read_data_set(data_file)
+        self.number_of_samples = self.data_set.shape[0]
+
+    def _read_data_set(self, filename):
+        """
+        Read the data set from a given file.
+
+        Parameters
+        ----------
+        filename : str
+            The path to the file containing the data set.
+
+        Returns
+        -------
+        array
+            The data set.
+        """
+        data_set = np.genfromtxt(
+            filename,
+            delimiter=",",
+            dtype=[np.uintc, np.uintc, np.uintc, np.double],
+            names=["p", "q", "r", "score"],
+        )
+        return data_set
 
     def _compute_weights(self):
-        sigma = np.zeros(self.number_of_samples)
-        for i in range(self.number_of_samples):
-            sigma[i] = 1.0 / (self.p[i]*self.q[i])
+        """
+        Compute the weights for the regression.
+        """
+        sigma = [1.0 / (p * q) for p, q in zip(self.data_set["p"], self.data_set["q"])]
         return sigma
 
-    def _fit_function(self):
-        self.popt, self.pcov = curve_fit(self.polynomial, (self.p, self.q, self.r), self.score, sigma=self._compute_weights(), absolute_sigma=True)
+    def _fit_function(self, function):
+        """
+        Fit a given function to the data set using scipy.optimize.curve_fit.
 
-    def _predict_y(self):
-        y_predict_data = []
-        for i in range(self.number_of_samples):
-            x = (self.p[i], self.q[i], self.r[i])
-            y_predict_data.append(self.polynomial(x, *self.popt))
-        self.predicted_y = np.array(y_predict_data)
+        Parameters
+        ----------
+        function : function
+            The function to fit.
 
-    def _mae(self, y_true, predictions):
-        y_true, predictions = np.array(y_true), np.array(predictions)
-        return np.mean(np.abs(y_true - predictions))
+        Returns
+        -------
+        tuple
+            The optimal parameters and the covariance matrix.
+        """
+        optimal_parameters, optimal_covariance = curve_fit(
+            function,
+            (self.data_set["p"], self.data_set["q"], self.data_set["r"]),
+            self.data_set["score"],
+            sigma=self._compute_weights(),
+            absolute_sigma=True,
+        )
 
-    def _compute_mae(self):
-        return self._mae(self.predicted_y , self.score)
+        return optimal_parameters, optimal_covariance
 
-    def _report(self):
-        print("*** REGRESSION REPORT ***\n")
-        print(f"Fitted function: {self.polynomial.__name__}")
-        print(f"Intercept: {self.popt[0]:E}")
-        print(f"Coeficients:\n{self.popt[1:]}\n")
-        print(f"Mean Absolute Error: {self._compute_mae():E}\n")
+    def _predict_y(self, function, optimal_parameters):
+        """
+        Predict the values of y for the data set.
 
-    def make_regression(self):
-        self._fit_function()
-        self._predict_y()
-        self._report()
+        Parameters
+        ----------
+        function : function
+            The function to fit.
+        optimal_parameters : array
+            The optimal parameters of the function.
 
-if __name__ == '__main__':
+        Returns
+        -------
+        array
+            The predicted values of y.
+        """
+        p = lambda x: function(x, *optimal_parameters)
+        x_data = zip(self.data_set["p"], self.data_set["q"], self.data_set["r"])
+        predicted_y = np.array([p(x) for x in x_data])
 
-    for score_dist in ["lublin-256-final-score.csv", "ctc-sp2-final-score.csv", "sdsc-blue-final-score.csv"]:
-        for heuristic in [lin, qdr, cub, qua, qui, sex]:
-            regressor = Regressor(score_dist, heuristic)
-            regressor.make_regression()
+        return predicted_y
+
+    def _compute_mae(self, predicted_y):
+        """
+        Compute the mean absolute error.
+
+        Parameters
+        ----------
+        predicted_y : array
+            The predicted values of y.
+
+        Returns
+        -------
+        float
+            The mean absolute error of the current regression.
+        """
+        return np.mean(np.abs(predicted_y - self.data_set["score"]))
+
+    def _generate_report(
+        self,
+        function,
+        optimal_parameters,
+        optimal_covariance,
+        predicted_y,
+        include_covariance,
+    ):
+        """
+        Generate a report of the regression for a given function.
+
+        Parameters
+        ----------
+        function : function
+            A function used for the regression.
+        optimal_parameters : array
+            The optimal parameters of the function.
+        optimal_covariance : array
+            The covariance matrix of the function.
+        predicted_y : array
+            The predicted values of y.
+        include_covariance : bool
+            Whether to include the covariance matrix in the report.
+
+        Returns
+        -------
+        dict
+            The report of the regression.
+        """
+        report = {
+            "fitted_function": function.__name__,
+            "coeficients": optimal_parameters.tolist(),
+            "mean_absolute_error": self._compute_mae(predicted_y),
+        }
+
+        if include_covariance:
+            report["covariance"] = optimal_covariance.tolist()
+
+        return report
+
+    def regression(self, output_file, include_covariance=False):
+        """
+        Perform the regression on the data set and write the report to a file.
+
+        Parameters
+        ----------
+        output_file : str
+            The path to the file where the report will be written.
+        include_covariance : bool, optional
+            Whether to include the covariance matrix in the report.
+        """
+        reports = []
+        for function in self.functions:
+            optimal_parameters, optimal_covariance = self._fit_function(function)
+            predicted_y = self._predict_y(function, optimal_parameters)
+            report = self._generate_report(
+                function,
+                optimal_parameters,
+                optimal_covariance,
+                predicted_y,
+                include_covariance,
+            )
+            reports.append(report)
+
+        with open(output_file, "w") as f:
+            json.dump(reports, f, indent=4)
+
+
+def check_and_download_data():
+    for file in DOWNLOADABLE_DATA:
+        file_id = file["file-id"]
+        file_name = file["file-name"]
+
+        if not os.path.exists(file_name):
+            gdown.download(
+                id=file_id,
+                output=file_name,
+                quiet=True,
+            )
+
+
+if __name__ == "__main__":
+    print("Checking if the data is downloaded...")
+    check_and_download_data()
+
+    print("Performing the regression...")
+    regressor = Regressor(SCORE_DISTRIBUTION, FUNCTIONS)
+    regressor.regression(REPORT_FILE)
+
+    print("Done!")
